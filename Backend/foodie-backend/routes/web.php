383cc78baf25c9e3c5a7fa67cab13b556457b005
tanmoybiswas478 <sranit55@gmail.com>
2026-08-19 15:37:09 +0530

    Ager tate nisiga.

--- Backend/foodie-backend/routes/web.php
+++ Backend/foodie-backend/routes/web.php
@@ -1,15 +1,7 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
-// Tumhare baaki ke web routes yahan honge (agar koi hain toh)
-
-// Sabse last mein yeh catch-all route daalo
-Route::get('/{any?}', function () {
-    return File::get(public_path('index.html'));
-})->where('any', '.*');
+Route::get('/', function () {
+    return view('welcome');
+});
